3d8f8adcb71594600c7bb48231d1bc913f294152
jakub.kolacek <jakub.kolacek@ibacz.eu>
2025-05-04 12:48:16 +0200

    The basic ConnectorObject annotation

---
+++ codegen-core/src/main/java/dev/kolacek/midpoint/codegen/annotation/ConnectorObject.java
@@ -0,0 +1,11 @@
+package dev.kolacek.midpoint.codegen.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.CLASS)
+@Target(ElementType.TYPE)
+public @interface ConnectorObject {
+}
